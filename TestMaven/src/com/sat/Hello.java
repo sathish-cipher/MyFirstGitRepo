11190e3d2e499a3ce660e0f473c6f0381f6d3c1a
Sathish R <Sathish R@Sathish>
2020-08-02 19:11:18 +0530

    FirstBranch Commit

--- TestMaven/src/com/sat/Hello.java
+++ TestMaven/src/com/sat/Hello.java
@@ -1,12 +1,11 @@
 package com.sat;
 
-import java.text.NumberFormat;
 import java.time.LocalDateTime;
-import java.util.Locale;
 
 public class Hello {
 		public static void main(String[] args) {
 			LocalDateTime dt=LocalDateTime.now();
 			System.out.println(dt.toLocalDate());
+			System.out.println("Hello");
 	}
 }
